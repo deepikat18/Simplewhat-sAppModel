2ac339d4f7f985273a7a2f3411ea5b22fc6a3d5b
Deepika <deepikathiruvallamgadu@gmail.com>
2023-05-07 11:48:10 +0530

    TabFragments

---
+++ app/src/main/java/com/example/tabfragmentexample/ViewPagerMessengerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.tabfragmentexample;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ViewPagerMessengerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerMessengerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if(position==0){
+            return new CheckFragment();
+        } else if (position==1) {
+            return new StatusFragment();
+        }
+        else{
+            return new CallsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;//no of tab count
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position==0){
+            return "Chats";
+        } else if (position ==1) {
+            return "Status";
+
+        }
+        else
+            return "Calls";
+    }
+}
